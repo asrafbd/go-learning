7a53472b2895440821f007c03bd1865287bf4136
Asraful Islam <asrafbd2323@gmail.com>
2024-11-27 19:37:48 +0600

    updated

--- forloopexample.go
+++ forloopexample.go
@@ -5,9 +5,15 @@ import "fmt"
 func main() {
 
 	var fruits = []string{"Mango", "Banana", "Apple", "Orange"}
-
 	for i := 0; i < len(fruits); i++ {
 
+		fmt.Println(fruits[i])
+	}
+
+	// 2nd method for loop
+
+	fmt.Println("2nd Method")
+	for i := range fruits {
 		fmt.Println(fruits[i])
 	}
 
